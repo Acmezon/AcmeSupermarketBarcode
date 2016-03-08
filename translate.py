1f097ad1f638588646e5b1da545e8fbfe07ea454
Ale Sánchez <alesanchezmedina@gmail.com>
2016-03-08 14:20:46 +0100

    corregido el formato

--- translate.py
+++ translate.py
@@ -19,6 +19,7 @@ def translate_to_binary(lines_spaces_vector):
 
     return r
 
+
 def translate_byte(binary_code, side):
     """
     Translate a single 7-bits code to its number code format.
@@ -29,51 +30,56 @@ def translate_byte(binary_code, side):
     bin_string = ""
 
     for x in binary_code:
-        bin_string+=str(int(x))
+        bin_string += str(int(x))
 
     if (side == 0):
         left_dict = {
-                '0001101': 0,
-                '0011001': 1,
-                '0010011': 2,
-                '0111101': 3,
-                '0100011': 4,
-                '0110001': 5,
-                '0101111': 6,
-                '0111011': 7,
-                '0110111': 8,
-                '0001011': 9
+            '0001101': 0,
+            '0011001': 1,
+            '0010011': 2,
+            '0111101': 3,
+            '0100011': 4,
+            '0110001': 5,
+            '0101111': 6,
+            '0111011': 7,
+            '0110111': 8,
+            '0001011': 9
         }
         return left_dict[bin_string]
     elif (side == 1):
         right_dict = {
-                '1110010': 0,
-                '1100110': 1,
-                '1101100': 2,
-                '1000010': 3,
-                '1011100': 4,
-                '1001110': 5,
-                '1010000': 6,
-                '1000100': 7,
-                '1001000': 8,
-                '1110100': 9
+            '1110010': 0,
+            '1100110': 1,
+            '1101100': 2,
+            '1000010': 3,
+            '1011100': 4,
+            '1001110': 5,
+            '1010000': 6,
+            '1000100': 7,
+            '1001000': 8,
+            '1110100': 9
         }
         return right_dict[bin_string]
     else:
         raise ValueError("Side param incorrect.")
-    
+
+
 def checksum(barcode):
     # 1. Add the values of the digits in positions 1, 3, 5, 7, 9, and 11.
-    term1 = int(barcode[0]) + int(barcode[2]) + int(barcode[4]) + int(barcode[6]) + int(barcode[8]) + int(barcode[10])
+    term1 = int(barcode[0]) + int(barcode[2]) + int(barcode[4]) + \
+            int(barcode[6]) + int(barcode[8]) + int(barcode[10])
     # 2. Multiply this result by 3.
     term2 = 3 * term1
     # 3. Add the values of the digits in positions 2, 4, 6, 8, and 10.
-    term3 = int(barcode[1]) + int(barcode[3]) + int(barcode[5]) + int(barcode[7]) + int(barcode[9])
+    term3 = int(barcode[1]) + int(barcode[3]) + \
+        int(barcode[5]) + int(barcode[7]) + int(barcode[9])
     # 4. Sum the results of steps 2 and 3.
     term4 = term2 + term3
-    # 5. The check character is the smallest number which, when added to the result in step 4, produces a multiple of 10.
-    term5 = ((10-term4)%10)%10
-    return str(term5)==barcode[-1]
+    # 5. The check character is the smallest number which, when added to the
+    # result in step 4, produces a multiple of 10.
+    term5 = ((10 - term4) % 10) % 10
+    return str(term5) == barcode[-1]
+
 
 def translate(vector):
     """
@@ -117,7 +123,8 @@ def translate(vector):
                 translateR += str(translate_byte(right, 1))
 
             else:
-                print("Error: Group " + str(group) + " wrong format. Parity not matching.")
+                print("Error: Group " + str(group) +
+                      " wrong format. Parity not matching.")
                 return -1
 
         barcode = translateL + translateR
@@ -128,7 +135,9 @@ def translate(vector):
             return -1
 
     else:
-        print("Error: Wrong format. Incorrent barcode length or frontier bars not matching.")
+        print(
+            "Error: Wrong format. Incorrent barcode \
+            length or frontier bars not matching.")
         return -1
 
 
@@ -143,4 +152,3 @@ y = translate(vector)
 
 print('Tarda ' + str(current_milli_time() - t) + ' s.')
 print('Resultado: ' + y)
-
